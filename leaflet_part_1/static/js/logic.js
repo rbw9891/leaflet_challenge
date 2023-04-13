cb5f82987f3d87321dbb1592e09dec914f38e5a0
rbw9891 <rbw9891@gmail.com>
2023-04-13 11:42:44 -0400

    radius of circles work, need function for color

--- leaflet_part_1/static/js/logic.js
+++ leaflet_part_1/static/js/logic.js
@@ -6,28 +6,51 @@ var geoUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.
 // get data from url
 d3.json(geoUrl).then(function (data) {
     console.log(data.features);
+    createQuakes(data.features);
 });
 
 // apply data to markers on map
 function createQuakes(earthquakeData) {
+
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><h4>Magnitude: ${feature.properties.mag}</h4>`);
+    }
+
+    function createMarkers(feature, latlng) {
+        let options = {
+            radius: feature.properties.mag*3,
+            color: "orange",
+            fillColor: "orange",
+            opacity: 0.8
+        }
+        return L.circleMarker(latlng,options)
+    };
+    
+
     var quakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature
+        onEachFeature: onEachFeature,
+        pointToLayer: createMarkers
     });
 
     createMap(quakes);
 }
 
 function createMap(quakes) {
-    // create map
-    var myMap = L.map('map', {
-        center: [39.738735315486785, -105.01372654942192],
-        zoom: 4
-    });
+   
 
     // create layer to use for map
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    var topo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    }).addTo(myMap);
+    });
+
+    // create map
+    L.map('map', {
+        center: [39.738735315486785, -105.01372654942192],
+        zoom: 4,
+        layers: [topo, quakes]
+    });
+
+    
 }
 // formatting for all things
     // magnitude = size
