016c25d03136c630e9cd993cb459d729552ee798
rbw9891 <rbw9891@gmail.com>
2023-04-13 14:17:45 -0400

    final and readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # leaflet_challenge
+
+Here in lies the leaflet challenge respository for module 15:
+
+## Background
+
+In this assignment we built a leaflet map utilizing D3, Javascript, and HTML. We used data from the USGS to plot earthquake occurences across the globe and their corresponding data. I chose to use the data on earthquakes in the past day which registered higher than 1.0 magnitude. The size of the circle marker relates to the magnitude of the earthquake and the color corresponds to the depth from the surface in km.
+
+## Contents
+
+### leaflet_part_1
+
+#### static
+    - css
+        > style.css
+    - js
+        > leaflet.legend.js (script for plugin)
+        > logic.js
+
+#### index.html
+
+This README
--- leaflet_part_1/static/js/logic.js
+++ leaflet_part_1/static/js/logic.js
@@ -1,37 +1,49 @@
 // geojson url
-var geoUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
+var geoUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson";
 
-
+// function for color gradient
+function chooseColor(depth) {
+    if (depth < 10.0) return "#90ee90";
+    else if (depth >= 10.0 && depth < 30.0) return "#9acd32"; 
+    else if (depth >= 30.0 && depth < 50.0) return "#fab733"; 
+    else if (depth >= 50.0 && depth < 70.0) return "#ff8e15"; 
+    else if (depth >= 70.0 && depth < 90.0) return "#ff4e11"; 
+    else return "#ff0d0d";
+}
 
 // get data from url
 d3.json(geoUrl).then(function (data) {
-    console.log(data.features);
+    // console.log(data.features);
+    // call createQuakes function to parse GeoJSON data appropriately and display
     createQuakes(data.features);
 });
 
 // apply data to markers on map
 function createQuakes(earthquakeData) {
 
+    // onEachFeature function
     function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><h4>Magnitude: ${feature.properties.mag}</h4>`);
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><h4>Magnitude: ${feature.properties.mag}</h4><h4>Depth: ${feature.geometry.coordinates[2]}`);
     }
 
+    // pointToLayer function
     function createMarkers(feature, latlng) {
-        let options = {
+        var options = {
             radius: feature.properties.mag*3,
-            color: "orange",
-            fillColor: "orange",
-            opacity: 0.8
+            color: chooseColor(feature.geometry.coordinates[2]),
+            fillColor: chooseColor(feature.geometry.coordinates[2]),
+            opacity: 0.9
         }
         return L.circleMarker(latlng,options)
     };
     
-
+    // variable that uses leaflet geoJSON method and functions created above
     var quakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature,
         pointToLayer: createMarkers
     });
 
+    // create the map with function and geoJSON data from above
     createMap(quakes);
 }
 
@@ -44,16 +56,50 @@ function createMap(quakes) {
     });
 
     // create map
-    L.map('map', {
+    var myMap = L.map('map', {
         center: [39.738735315486785, -105.01372654942192],
-        zoom: 4,
+        zoom: 3,
         layers: [topo, quakes]
     });
 
+    // create legend and add to map
+    var legend = L.control.legend({
+        position: "bottomleft",
+        title: "Earthquake Depth",
+        opacity: 0.7,
+        legends: [{
+            label: "<10 km",
+            type: "circle",
+            color: "#90ee90"
+        },
+                {
+            label: "10-30 km",
+            type: "circle",
+            color: "#9acd32"
+                },
+                {
+            label: "30-50 km",
+            type: "circle",
+            color: "#fab733"
+                },
+                {
+            label: "50-70 km",
+            type: "circle",
+            color: "#ff8e15"
+                },
+                {
+            label: "70-90 km",
+            type: "circle",
+            color: "#ff4e11"
+                },
+                {
+            label: "90+ km",
+            type: "circle",
+            color: "#ff0d0d"
+                }]
+    });
+
+    legend.addTo(myMap);
+
     
 }
-// formatting for all things
-    // magnitude = size
-    // depth = color (deeper = darker)
-    // popups on click w/ additional info
-    // legend
